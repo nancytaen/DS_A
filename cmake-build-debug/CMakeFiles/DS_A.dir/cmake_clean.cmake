f036ddc2f6ac587ceac6f1fb0f9599968699e4b0
nancytaen <taen.heizo8@gmail.com>
2021-11-16 21:45:26 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/DS_A.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DS_A.dir/main.cpp.obj"
+  "DS_A.exe"
+  "DS_A.exe.manifest"
+  "DS_A.pdb"
+  "libDS_A.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DS_A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
